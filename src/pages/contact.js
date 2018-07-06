b2458733f41003787ce0bcbaa91da4cfbd39d98f
David Berget <davidberget@gmail.com>
2018-07-06 13:31:01 -0500

    tweaking netlify form again

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,67 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { Form, Button, Container, Input, Icon } from 'semantic-ui-react'
+import { Form, Container, Input, Icon } from 'semantic-ui-react'
 
-const Contact = () => (
-  <Container text>
-    <Helmet title="About" />
-    <Form name="contact" method="POST" netlify>
-      <Form.Group widths="equal">
-        <Form.Field>
-          <Input iconPosition="left" placeholder="Name">
-            <Icon name="user" />
-            <input name="name" />
-          </Input>
-        </Form.Field>
-        <Form.Field>
-          <Input iconPosition="left" placeholder="Email">
-            <Icon name="at" />
-            <input name="email" />
-          </Input>
-        </Form.Field>
-      </Form.Group>
-      <Form.Group widths="equal">
-        <Form.Field>
-          <Input iconPosition="left" placeholder="Phone Number">
-            <Icon name="phone" />
-            <input name="phone" />
-          </Input>
-        </Form.Field>
-      </Form.Group>
-      <Form.TextArea label="Message" name="message" />
-      <Form.Button type="submit">Submit</Form.Button>
-    </Form>
-  </Container>
-)
+class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    })
+  }
+
+  handleSubmit = () => {
+    fetch(`/`, {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+  }
+
+  render() {
+    return (
+      <Container text>
+        <Helmet title="About" />
+        <Form
+          onSubmit={() => this.handleSubmit}
+          name="contact"
+          method="POST"
+          netlify
+        >
+          <Form.Group widths="equal">
+            <Form.Field>
+              <Input iconPosition="left" placeholder="Name">
+                <Icon name="user" />
+                <input name="name" />
+              </Input>
+            </Form.Field>
+            <Form.Field>
+              <Input iconPosition="left" placeholder="Email">
+                <Icon name="at" />
+                <input name="email" />
+              </Input>
+            </Form.Field>
+          </Form.Group>
+          <Form.Group widths="equal">
+            <Form.Field>
+              <Input iconPosition="left" placeholder="Phone Number">
+                <Icon name="phone" />
+                <input name="phone" />
+              </Input>
+            </Form.Field>
+          </Form.Group>
+          <Form.TextArea label="Message" name="message" />
+          <Form.Button type="submit">Submit</Form.Button>
+        </Form>
+      </Container>
+    )
+  }
+}
 
 export default Contact
