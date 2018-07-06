5d75db6ef210f8dd221075b93509baffcdd0e7f8
David Berget <davidberget@gmail.com>
2018-07-06 12:59:53 -0500

    adding netlify forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,15 +5,15 @@ import { Form, Button, Container, Input, Icon } from 'semantic-ui-react'
 const Contact = () => (
   <Container text>
     <Helmet title="About" />
-    <Form name="contact" method="POST" data-netlify="true">
+    <Form name="contact" method="POST" netlify>
       <Form.Group widths="equal">
         <Form.Field>
-          <Input iconPosition="left" placeholder="Name">
+          <Input iconPosition="left" name="name" placeholder="Name">
             <Icon name="user" />
             <input />
           </Input>
         </Form.Field>
-        <Form.Field>
+        <Form.Field name="email">
           <Input iconPosition="left" placeholder="Email">
             <Icon name="at" />
             <input />
